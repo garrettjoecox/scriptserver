125b60f7cf1d4582a70375923e6e6ec42f01c5f8
Garrett Cox <garrettjcox@gmail.com>
2021-04-19 13:20:08 -0500

    Implement @scriptserver/command

---
+++ packages/command/src/index.ts
@@ -0,0 +1,70 @@
+import { ScriptServer } from '@scriptserver/core';
+import defaultsDeep from 'lodash.defaultsdeep';
+
+export interface CommandConfig {
+  prefix: string;
+}
+
+interface EventPayload {
+  player: string;
+  command: string;
+  args?: string[];
+}
+
+type EventHandler = (event: EventPayload) => void;
+
+declare module '@scriptserver/core' {
+  export interface ScriptServerEvents {
+    command: EventHandler;
+  }
+
+  export interface ScriptServerConfig {
+    command: CommandConfig;
+  }
+
+  export interface ScriptServer {
+    command(command: string, callback: EventHandler): void;
+  }
+}
+
+export const DEFAULT_COMMAND_CONFIG: CommandConfig = {
+  prefix: '~',
+};
+
+export default (server: ScriptServer) => {
+  server.config.command = defaultsDeep(server.config.command, DEFAULT_COMMAND_CONFIG);
+
+  const regex = new RegExp(`^${server.config.command.prefix}([\\w]+)\\s?(.*)`, 'i');
+  const commands: {
+    [commandName: string]: EventHandler[];
+  } = {};
+
+  server.command = (command, callback) => {
+    command = command.toLowerCase();
+    commands[command] = commands[command] || [];
+    commands[command].push(callback);
+  };
+
+  // @ts-ignore
+  server.on('chat', event => {
+    const stripped: null | string[] = event.message.match(regex);
+    if (stripped) {
+      const command = stripped[1].toLowerCase();
+      server.emit('command', {
+        player: event.player,
+        command,
+        args: stripped[2].split(' '),
+      });
+    }
+  });
+
+  server.on('command', event => {
+    if (commands.hasOwnProperty(event.command)) {
+      commands[event.command].forEach(callback => {
+        Promise.resolve()
+          .then(() => callback(event))
+          .catch(error => console.error(error));
+      });
+    }
+  });
+};
