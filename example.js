a09a0c4e8b81e0c1e064e5e3bd50912dc3510c75
Garrett Cox <garrettjcox@gmail.com>
2021-04-19 13:20:39 -0500

    Implement ScriptServer class

--- example.js
+++ example.js
@@ -1,13 +1,34 @@
-const { JavaServer, RconConnection } = require('./packages/core');
+const { JavaServer, RconConnection, ScriptServer } = require('./packages/core');
+const event = require('./packages/event').default;
+const command = require('./packages/command').default;
 
-const server = new JavaServer();
-const rconConnection = new RconConnection();
+// const server = new JavaServer();
+// const rconConnection = new RconConnection();
 
-server.on('console', console.log);
-server.on('started', () => rconConnection.connect());
+// server.on('console', console.log);
+// server.on('started', () => rconConnection.connect());
 
-server.start();
+// server.start();
 
-rconConnection.on('connected', async () => {
-  console.log(await rconConnection.send('help'));
+// rconConnection.on('connected', async () => {
+//   console.log(await rconConnection.send('help'));
+// });
+
+const scriptServer = new ScriptServer();
+
+event(scriptServer);
+command(scriptServer);
+
+scriptServer.start();
+
+scriptServer.on('console', console.log);
+scriptServer.on('started', () => {
+  console.log('Started!');
+});
+scriptServer.on('login', event => console.log('login', event));
+scriptServer.on('logout', event => console.log('logout', event));
+scriptServer.on('chat', event => console.log('chat', event));
+scriptServer.on('command', event => console.log('command', event));
+scriptServer.command('test', event => {
+  console.log('test', event);
 });
--- packages/core/src/ScriptServer.ts
+++ packages/core/src/ScriptServer.ts
@@ -1,1 +1,53 @@
-export class ScriptServer {}
+import EventsEmitter from 'events';
+import { DEFAULT_JAVA_SERVER_CONFIG, JavaServer, JavaServerConfig } from './JavaServer';
+import { DEFAULT_RCON_CONNECTION_CONFIG, RconConnection, RconConnectionConfig } from './RconConnection';
+
+export interface ScriptServerEvents {
+  console: (message: string) => void;
+  started: () => void;
+  stopped: () => void;
+}
+
+export declare interface ScriptServer {
+  on<U extends keyof ScriptServerEvents>(event: U, listener: ScriptServerEvents[U]): this;
+  emit<U extends keyof ScriptServerEvents>(event: U, ...args: Parameters<ScriptServerEvents[U]>): boolean;
+}
+
+export interface ScriptServerConfig {
+  core: {
+    javaServer: JavaServerConfig;
+    rconConnection: RconConnectionConfig;
+  };
+}
+
+export const DEFAULT_SCRIPT_SERVER_CONFIG: ScriptServerConfig = {
+  core: {
+    javaServer: DEFAULT_JAVA_SERVER_CONFIG,
+    rconConnection: DEFAULT_RCON_CONNECTION_CONFIG,
+  },
+};
+
+export class ScriptServer extends EventsEmitter {
+  public config: ScriptServerConfig = DEFAULT_SCRIPT_SERVER_CONFIG;
+  private javaServer: JavaServer;
+  private rconConnection: RconConnection;
+
+  constructor(config: Partial<ScriptServerConfig> = {}) {
+    super();
+
+    Object.assign(this.config, config);
+    this.javaServer = new JavaServer(this.config.core.javaServer);
+    this.rconConnection = new RconConnection(this.config.core.rconConnection);
+
+    this.javaServer.on('console', message => this.emit('console', message));
+    this.javaServer.on('stopped', () => this.emit('stopped'));
+    this.rconConnection.on('connected', () => this.emit('started'));
+  }
+
+  public start() {
+    this.javaServer.start();
+    this.javaServer.once('started', () => {
+      this.rconConnection.connect();
+    });
+  }
+}
